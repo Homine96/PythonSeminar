0a99b95feb3b1700ca1063edcda5dde635d83b73
Pavel <Shestakov1996@yandex.ru>
2022-12-17 23:30:22 +0300

    Задачки с разбором из 2 семинара

---
+++ Sem2/Task2_1.py
@@ -0,0 +1,12 @@
+# напишите программу, которая принимает
+# на вход число N  и выдает последовательность
+# из N  членов
+
+n= int(input())
+# list=[]
+# for i in range(n):
+#     x=(-3)**i
+#     list.append(x)
+# print(list)    
+for i in range(5):
+    print((-3)**i,end=' ')
