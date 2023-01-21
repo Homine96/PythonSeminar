1c3c700e1f912de885614a88268e4072405cb4eb
Pavel <Shestakov1996@yandex.ru>
2023-01-21 21:49:36 +0300

    delete file

--- TelefonDirectory/write_col.py
+++
@@ -1,4 +0,0 @@
-import csv
-with open("telefdirect1.csv", mode="w", encoding='utf-8') as file:
-    file_writer = csv.writer(file, delimiter = "|", lineterminator="\r")
-    file_writer.writerow(["ID", "Name", "Surname", 'Number', 'Comment'])
